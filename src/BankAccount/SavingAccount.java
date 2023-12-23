b417ad2dcb6ee72587f5e1768cc40929b8b20a25
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2023-12-23 18:26:46 -0500

    Update SavingAccount.java

--- src/BankAccount/SavingAccount.java
+++ src/BankAccount/SavingAccount.java
@@ -1,5 +1,8 @@
 package BankAccount;
 
+/**
+* Savings Account type, subclass of BankAccount*/
+
 import Customer.Customer;
 import Enum.AccountType;
 
@@ -16,18 +19,22 @@ public class SavingAccount extends BankAccount {
         this.term = term;
     }
 
+    /*Retrieve interest rate of a savings account*/
     public double getInterestRate() {
         return interestRate;
     }
 
+    /*Set interest rate of a savings account*/
     public void setInterestRate(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    /*Check if the term of a savings account has elapsed or not*/
     public boolean isEarly() {
         return isEarly;
     }
 
+    /*Determine whether the term of a savings account has matured*/
     public void setIsEarly(double term) {
         if(Double.parseDouble(this.term) > term){
             this.isEarly = true;
