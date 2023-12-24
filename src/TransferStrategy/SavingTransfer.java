1fdb399f2371b3e734a71c4cc18cc3676b095b70
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2023-12-23 20:06:56 -0500

    Update SavingTransfer.java

--- src/TransferStrategy/SavingTransfer.java
+++ src/TransferStrategy/SavingTransfer.java
@@ -28,25 +28,6 @@ public class SavingTransfer implements TransferStrategy{
     }
 
     public void transfer(BankAccount initialAccount, BankAccount targetAccount, double amount) throws Exception{
-        /*if(amount>Double.parseDouble(initialAccount.getBalance())){
-            throw new MinimumBalanceException("Error: Cannot transfer more than balance!");
-        }else if(isTransferLimit(amount)){
-            throw new ExcessTransferException("Error: Transferring amount exceeds daily limit!");
-        }else if (!(initialAccount instanceof SavingAccount)){
-            throw new InvalidInputException("Error: Choose the wrong transfer method!");
-        }else if(!(targetAccount instanceof SavingAccount)){
-            throw new InvalidInputException("Error: Saving account is only allowed to transfer to check account!");
-        }else{
-            double newBalanceInitial = Double.parseDouble(initialAccount.getBalance())-amount;
-            
-            if(((SavingAccount)initialAccount).isEarly()){
-                newBalanceInitial -=amount*EarlyPenalty.FIVEPERCENT.getValue();
-            }
-            initialAccount.setBalance(String.valueOf(newBalanceInitial));
-
-            double newBalanceTarget = Double.parseDouble(targetAccount.getBalance()) + amount;
-            targetAccount.setBalance(String.valueOf(newBalanceTarget));
-        }*/
         if (!(initialAccount instanceof SavingAccount)){ 
             throw new InvalidInputException("Error: Choose the wrong transfer method!");
         }else{
