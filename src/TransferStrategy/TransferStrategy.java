3a501e056563acb7681a5dcb6580c09a6ba5f438
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2023-12-23 20:17:05 -0500

    Update TransferStrategy.java

--- src/TransferStrategy/TransferStrategy.java
+++ src/TransferStrategy/TransferStrategy.java
@@ -1,8 +1,14 @@
 package TransferStrategy;
 
+/**
+*TransferStrategy interface implementable by other classes to customize methods for determining excess of transfer limit
+*and execute transfer activity*/
+
 import BankAccount.BankAccount;
 
 public interface TransferStrategy {
+    /*Determine whether the amount to be transferred is over limit*/
     boolean isTransferLimit(double amount);
+    /*Carry out transfer transaction from the initial account to target account*/
     void transfer(BankAccount initialAccount, BankAccount targetAccount,double amount)throws Exception;
 }
