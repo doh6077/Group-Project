f6eb2280323a71961a5c5d32605d374b30233b77
Claire-cpu <67937914+Claire-cpu@users.noreply.github.com>
2023-12-27 13:57:18 -0500

    Update DepositStrategy.java

--- src/DepositStrategy/DepositStrategy.java
+++ src/DepositStrategy/DepositStrategy.java
@@ -1,5 +1,10 @@
 package DepositStrategy;
 import BankAccount.*;
+
+/**
+*DepositStrategy interface implementable by other classes to customize methods for executing transfer activity*/
 public interface DepositStrategy {
+
+     /*Carry out deposit transaction from the initial account to target account*/
     void deposit(BankAccount account, double amount)throws Exception;
 }
