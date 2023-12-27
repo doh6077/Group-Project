d2812c4455ba051eb273878082bb80776e195dcc
Claire-cpu <67937914+Claire-cpu@users.noreply.github.com>
2023-12-27 14:01:26 -0500

    Update CheckDeposit.java

--- src/DepositStrategy/CheckDeposit.java
+++ src/DepositStrategy/CheckDeposit.java
@@ -3,11 +3,18 @@ package DepositStrategy;
 import BankAccount.*;
 import Exception.*;
 
+/**
+*Concrete deposit strategy class for Checking Account class implements the interface of DepositStrategy
+*Provide methods to assess if the deposit on a checking account is within the valid limit
+*Provide concrete deposit method for checking account*/
 public class CheckDeposit implements DepositStrategy {
     public CheckDeposit(){
 
     }
 
+    /*Check if the amount entered by user is in valid numeric format
+    *@param String str: amount input by user
+    *@return boolean: true if input is in valid numeric format otherwise return false*/
     public boolean isNumeric(String str) {
         try {
             Double.parseDouble(str);
@@ -17,6 +24,11 @@ public class CheckDeposit implements DepositStrategy {
         }
     }
 
+    /*Deposit method for checking account  
+    *@param BankAccount account: account for money deposit
+    *@param double amount: amount of money to deposit
+    *@return boolean: void
+    *@throw exception: InvalidInputException*/
     public void deposit(BankAccount account, double amount)throws Exception{
 
         if (!(account instanceof CheckAccount)){ 
