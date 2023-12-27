9c8b7d1bf2fa5b3290ccbfca048a316ddd9414c3
Claire-cpu <67937914+Claire-cpu@users.noreply.github.com>
2023-12-27 15:47:09 -0500

    Update CheckWithdraw.java

--- src/WithdrawStrategy/CheckWithdraw.java
+++ src/WithdrawStrategy/CheckWithdraw.java
@@ -6,7 +6,7 @@ import Exception.*;
 
 /**
 *Concrete withdrawal strategy class for Checking Account class implements the interface of WithdrawalStrategy
-*Provide methods to assess if withdrawal from checking account is within the valid limit
+*Provide methods to assess if withdrawal from a checking account is within the valid limit
 *Provide concrete withdrawal method for checking account*/
 public class CheckWithdraw implements WithdrawStrategy{
 
@@ -14,9 +14,9 @@ public class CheckWithdraw implements WithdrawStrategy{
 
     }
 
-    /*Check if the amount entered by user is over the withdrawal limit
-    *@param double amount: amount to be withdrawn by user
-    *@return boolean: true if the transfer is within withdrawal limit otherwise return false*/
+    /*Check if the amount entered by the user is over the withdrawal limit
+    *@param double amount: amount to be withdrawn by the user
+    *@return boolean: true if the transfer is within the withdrawal limit otherwise return false*/
     public boolean isWithdrawLimit(double amount){
         if(amount>WithdrawLimit.TWOTHOUSAND.getValue()){
             return true;
@@ -27,7 +27,7 @@ public class CheckWithdraw implements WithdrawStrategy{
 
      /*Withdrawal method for checking account to withdraw money from one account to another account with validation of 
     *appropriate numeric format and minimum and maximum balance requirements
-    *@param BankAccount account: account where money is withdrawn from
+    *@param BankAccount account: checking account where money is withdrawn from
     *@param double amount: amount of money to withdraw
     *@return boolean: void
     *@throw exception: InvalidInputException, MinimumBalanceException, ExcessTransferException*/
