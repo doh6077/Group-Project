ac4f2ed60f00415f489db8e010bbf8bc5f8b3722
Claire-cpu <67937914+Claire-cpu@users.noreply.github.com>
2023-12-27 14:03:48 -0500

    Update SavingDeposit.java

--- src/DepositStrategy/SavingDeposit.java
+++ src/DepositStrategy/SavingDeposit.java
@@ -3,11 +3,18 @@ import BankAccount.*;
 import Enum.MiniDeposit;
 import Exception.*;
 
+/**
+*Concrete deposit strategy class for Savings Account class implements the interface of DepositStrategy
+*Provide methods to assess if the deposit on a Savings Account is within the valid limit
+*Provide concrete deposit method for Savings Account*/
 public class SavingDeposit implements DepositStrategy{
     public SavingDeposit(){
 
     }
 
+    /*Check if the amount entered by user is in valid numeric format
+    *@param String str: amount input by user
+    *@return boolean: true if input is in valid numeric format otherwise return false*/
     public boolean isNumeric(String str) {
         try {
             Double.parseDouble(str);
@@ -17,6 +24,11 @@ public class SavingDeposit implements DepositStrategy{
         }
     }
 
+    /*Deposit method for savings account  
+    *@param BankAccount account: account for money deposit
+    *@param double amount: amount of money to deposit
+    *@return boolean: void
+    *@throw exception: InvalidInputException, SavingThresholdException*/
     public void deposit(BankAccount account, double amount)throws Exception{
         System.out.println("saving deposit function get called");
         if (!(account instanceof SavingAccount)){
