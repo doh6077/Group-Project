827314f5518f7ee716aa3e704f78fb1aa1e57ad8
Claire-cpu <67937914+Claire-cpu@users.noreply.github.com>
2023-12-27 11:25:16 -0500

    Update TransferStrategy.java

--- src/TransferStrategy/TransferStrategy.java
+++ src/TransferStrategy/TransferStrategy.java
@@ -9,6 +9,7 @@ import BankAccount.BankAccount;
 public interface TransferStrategy {
     /*Determine whether the amount to be transferred is over limit*/
     boolean isTransferLimit(double amount);
+    
     /*Carry out transfer transaction from the initial account to target account*/
     void transfer(BankAccount initialAccount, BankAccount targetAccount,double amount)throws Exception;
 }
